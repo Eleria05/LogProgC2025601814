021041b10ef75db49fefa4c9e908775c36ab63ed
Eleria05 <jhernandeze2405@alumno.ipn.mx>
2025-04-07 08:36:16 -0600

    Update pizzas.c

--- Parcial2/Tarea2025-04-07_1300/pizzas.c
+++ Parcial2/Tarea2025-04-07_1300/pizzas.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
-int main(){
-  int pizzas,veg,clas;
-  printf("Bienvenido a pizzeria napoli \n Menu...\n1-Pizza clasica \n 2-Pizza vegetariana");
-  scanf("%i",&pizzas);
-  if(pizzas==1){
-    printf("Elija un ingrediente...\n1-Peperoni \n2-Jamon \n3-Salmon \n4-Clasica(tomate y mozzarella");
-  } 
-  if (clas=1){
-    printf("Su pizza sera con: Mozarrella,Tomate y Peperoni");
-  } else if (clas=2){
-        printf("Su pizza sera con: Mozarrella,Tomate y Jamon");
-  } else if (clas=3){
-   printf("Su pizza sera con: Mozarrella,Tomate y Salmon");
-  } else if (clas=3){
-    printf("Su pizza sera con: Mozarrella y Tomate");
-  }else {
-    printf("Error");
-  }
-   if (pizzas=2){
-    printf("Elija un ingrediente de su pizza vegetariana...\n1-Pimiento \n2-Tofu \n3-Clasica(Tomate y Mozzarella");
-   }
-   if (veg=1){
-    printf("Su pizza vegetariana sera de: Tomate ,Mozarrella y Pimiento ");
-   }else if (veg=2){
-    printf("Su pizza vegetariana sera de: Tomate ,Mozarrella y Tofu ");
-   }else if (veg=3){
-    printf("Su pizza vegetariana sera de: Clasica(Tomate y Mozzarella) ");
-   }else{
-    printf("Error");
-   }
-   
+
+int main() {
+    int vegetariana, clasica, pizzas;
+    pizzas = 0;
+    clasica = 0;
+    vegetariana = 0;
+    printf ("Bienvenido a Pizzeria Napoli..." );
+    printf ("MENU 1-Pizza Clasica  2-Pizza vegetariana" );
+    scanf ("%i",&pizzas);
+    if (pizzas == 2) {
+        printf("Elija un ingrediente....1-Pimiento 2-Tofu 3-Clasica");
+        scanf("%i",&vegetariana);
+        if (vegetariana == 1) {
+            printf("Los ingredientes de su pizza vegetariana seran: ...Mozarella,tomate y pimiento" );
+        } else {
+            if (vegetariana == 2) {
+                printf("Los ingredientes de su pizza vegetariana seran: ...Mozarella,tomate y tofu" );
+            } else {
+                if (vegetariana == 3) {
+                    printf("Los ingredientes de su pizza vegetariana seran: ...Mozarella y tomate" );
+                } else {
+                    printf("Error");
+                }
+            }
+        }
+    } else {
+        printf("Elija un ingrediente...1-Peperoni  2-Jamon 3-Salmon 4-Clasica" );
+        scanf("%i",&clasica);
+        if (clasica == 1) {
+            printf("Los ingredientes de su pizza seran: ...mozzarella,tomate y peperoni " );
+        } else {
+            if (clasica == 2) {
+                printf("Los ingredientes de su pizza seran: ...mozzarella,tomate y jamon " );
+            } else {
+                if (clasica == 3) {
+                    printf("Los ingredientes de su pizza seran: ...mozzarella,tomate y salmon " );
+                } else {
+                    if (clasica == 4) {
+                        printf("Los ingredientes de su pizza seran: ...mozzarella y tomate " );
+                    } else {
+                        printf( "Error");
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
